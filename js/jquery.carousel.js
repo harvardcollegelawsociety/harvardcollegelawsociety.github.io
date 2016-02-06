f0a2c0258d3c5433e6a0344725ad7fc9ee285f49
billiewei <billiewei@gmail.com>
2016-02-06 17:29:50 -0500

    Changed speed

--- js/jquery.carousel.js
+++ js/jquery.carousel.js
@@ -27,7 +27,7 @@
 
 					preload:						false,						// If true, preload fullsize images in the background
 					baseZIndex:						1000,						// Base Z-Index
-					fadeSpeed:						300,						// Global fade speed
+					fadeSpeed:						1200,						// Global fade speed
 					overlayColor:					'#000000',					// Overlay color
 					overlayOpacity:					0.6,						// Overlay opacity
 					overlayClass:					'carousel-overlay',			// Overlay class
